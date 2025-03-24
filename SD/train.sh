5a79b7f477fe03464d3b746c7bf4bb3dce6e6f54
Jing Wu <wwujing123@gmail.com>
2025-03-24 15:33:11 +1100

    Create train.sh

---
+++ SD/train.sh
@@ -0,0 +1,1 @@
+# python MUNBa_nsfw.py --config_path './configs/stable-diffusion/v1-inference_nash.yaml' --munba --class_to_forget '0' --train_method 'noxattn' --lr 1e-5 --epochs 1 --device '3' --batch_size 4 --beta 100.0 --with_l1 --alpha 1e-4
